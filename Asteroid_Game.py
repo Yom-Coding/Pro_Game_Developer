f886869fc3442ee4fcfad4d0b824fa3cffc4553c
Yom-Coding <yompatel88888888@gmail.com>
2026-02-21 12:07:04 +0000

    Add files via upload

--- Asteroid_Game.py
+++ Asteroid_Game.py
@@ -8,6 +8,21 @@ WIDTH = 600
 HEIGHT = 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Asteroids(pygame.sprite.Sprite):
+
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("/Users/yompatel/Desktop/Jet Learn/Pro Game Developer/Space Invaders/images_and_assets/image.png")
+        self.image = pygame.transform.scale(self.image, (50,50))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, WIDTH)
+        self.rect.y = 0
+
+
+
+
+
 background = pygame.image.load("/Users/yompatel/Desktop/Jet Learn/Pro Game Developer/image/space.jpg")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
@@ -33,10 +48,14 @@ asteroid_radius = 20
 asteroid_speed = 3
 
 last_spawn = pygame.time.get_ticks()
-spawn_delay = 2000 
+spawn_delay = 2000
 
 start_time = time.time()
 
+asteroid_group = pygame.sprite.Group()
+for i in range(5):
+    asteroid1 = Asteroids()
+    asteroid_group.add(asteroid1)
 
 
 
@@ -51,7 +70,7 @@ run = True
 while run:
     screen.blit(background, (0, 0))
     screen.blit(rocket, (rocket_rect.x, rocket_rect.y))
-
+    asteroid_group.draw(screen)
 
   
     mouse_x, mouse_y = pygame.mouse.get_pos()
